79358d9ec0ed54aaf63c11a3a5af349416d43db2
jurajosef <josef.jura@hotmail.com>
2015-11-26 16:18:28 +0100

    Added empty template to dashboard

--- app/main/controllers/dashboard-ctrl.js
+++ app/main/controllers/dashboard-ctrl.js
@@ -1,8 +1,20 @@
 'use strict';
 angular.module('main')
-.controller('DashboardCtrl', function ($log, Zomato, Config) {
+.controller('DashboardCtrl', function ($log, Zomato, Config, $localstorage) {
 
   $log.log('Hello from your Controller: DashboardCtrl in module main:. This is your controller:', this);
+
+    var config = $localstorage.getObject('config');
+    if (config.restaurants !== null) {
+      $log.log('Loaded configuration. Length: ' + config.restaurants.length);
+      this.restaurants = config.restaurants;
+    }
+    else {
+      $log.log('Creating new configuration');
+      this.restaurants = [];
+      config.restaurants = this.restaurants;
+      $localstorage.setObject('config', config);
+    }
 
   // bind data from services
   this.someData = Zomato.someData;
