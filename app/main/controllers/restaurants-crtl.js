53a17d69c9a059c584ecc62de890f2178dac7cdc
jurajosef <josef.jura@hotmail.com>
2015-11-27 13:33:04 +0100

    Added pull to refresh to restaurants screen

--- app/main/controllers/restaurants-crtl.js
+++ app/main/controllers/restaurants-crtl.js
@@ -1,12 +1,17 @@
 'use strict';
 angular.module('main')
-  .controller('RestaurantsCtrl', function ($log, Config, UserConfig) {
-
+  .controller('RestaurantsCtrl', function ($log, $scope, Config, UserConfig) {
+    var self = this;    
     $log.log('Hello from your Controller: RestaurantsCtrl in module main:. This is your controller:', this);
 
     this.restaurants = UserConfig.getRestaurantList();
 
     this.ENV = Config.ENV;
     this.BUILD = Config.BUILD;
+
+    this.refresh = function () {
+      self.restaurants = UserConfig.getRestaurantList();
+      $scope.$broadcast('scroll.refreshComplete');
+    }
 
   });
